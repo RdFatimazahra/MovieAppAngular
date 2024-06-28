12c085417a60717c0e6f265f24834dee82f54d14
RdFatimazahra <fatimaezahraradi@gmail.com>
2024-06-28 15:13:19 +0100

    Modification au niveau du fichier main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { appConfig } from './app/app.config';
+// import { AppComponent } from './app/app.component';
+
+// bootstrapApplication(AppComponent, appConfig)
+//   .catch((err) => console.error(err));
+
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(withFetch()) // Configure fetch
+  ]
+}).catch(err => console.error(err));
