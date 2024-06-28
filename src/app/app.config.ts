80f3940a480fe5c5dcb3a1403e5e9ed6eab26ae6
RdFatimazahra <fatimaezahraradi@gmail.com>
2024-06-28 15:07:15 +0100

    Modification au niveau du fichier app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,9 +7,9 @@ import { provideClientHydration } from '@angular/platform-browser';
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
 };*/
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http'; // Importer provideHttpClient
+import { HttpClientModule } from '@angular/common/http'; // Importer provideHttpClient
 import { provideClientHydration } from '@angular/platform-browser';
 
 import { routes } from './app.routes';
@@ -18,8 +18,10 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(), // Ajouter provideHttpClient ici
-    provideClientHydration()
+    // provideHttpClient(), // Ajouter provideHttpClient ici
+    provideClientHydration(),
+    importProvidersFrom(HttpClientModule)
+    
   ]
 };
 
