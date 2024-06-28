f1ca16bafca22f4379105e646a8c6289233e28ce
RdFatimazahra <fatimaezahraradi@gmail.com>
2024-06-28 15:14:55 +0100

    Modification au niveau du fichier app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet} from '@angular/router';
+// import { MovieService } from './Services/movie.service';
+import { Movie } from './models/movie';
+import { NgFor } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { response } from 'express';
+import { error } from 'console';
+
+// interface Movie {
+//   id: number;
+//   titre: string;
+//   dateSortie: string;
+//   genre: string;
+//   realisateur: string;
+//   note: number;
+// }
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor], // Ajoutez NgFor ici
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'first-app';
+  movies: Movie[] = [];
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit() {
+    
+    this.httpClient.get<Movie[]>("http://localhost:8082/api/movies").subscribe(response => {
+      this.movies = response;
+      console.log(this.movies);
+    },error =>{
+      console.log("eroor");
+    }
+  );
+  }
 }
